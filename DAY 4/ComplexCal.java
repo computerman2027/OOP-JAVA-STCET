b13d1eea6554508c2b24ef5fdd6ce5d5d7f9509d
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2025-08-07 10:28:17 +0530

    Day 4 files updated

--- DAY 4/ComplexCal.java
+++ DAY 4/ComplexCal.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 class Complex {
+    static int created =0, displayed =0;
     double real, imaginery;
     
     public Complex(double real, double imaginery)
     {
         this.real = real;
         this.imaginery= imaginery;
+
     }
     
     static Complex add(Complex a, Complex b)
@@ -36,10 +38,7 @@ class Complex {
     @Override
     public String toString() {
         return ""+real+" + "+imaginery+"i";
-    }
-
-    
-
+    }    
 }
 
 public class ComplexCal
@@ -52,19 +51,30 @@ public class ComplexCal
         System.out.println("Enter Imaginery part of 1st number");
         img=sc.nextDouble();
 
+        Complex.created++;
+        
         Complex obj1 = new Complex(real, img);
         System.out.println("Enter Real part of 2nd number");
         real=sc.nextDouble();
         System.out.println("Enter Imaginery part of 2nd number");
         img=sc.nextDouble();
-
+        
         Complex obj2 = new Complex(real, img);
+        
+        Complex.created++;
 
+        
         System.out.println("Complex num 1 = "+ obj1);
         System.out.println("Complex num 2 = "+ obj2);
         System.out.println("ADD = " + Complex.add(obj1, obj2));
+        Complex.displayed++;
         System.out.println("SUBSTRACTION = " + Complex.sub(obj1, obj2));
+        Complex.displayed++;
         System.out.println("MULTIPLY = " + Complex.mul(obj1, obj2));
+        Complex.displayed++;
+
+        System.out.println("Complex no created : "+Complex.created);
+        System.out.println("Complex no displayed : "+Complex.displayed);
 
 
     }
